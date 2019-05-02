4e38c5d70f4236ae5afa85f2afe12009a2562ef9
felfel <a.3lyounis@gmail.com>
2019-05-03 01:40:39 +0200

    add annotation loader

--- src/ConfigLoader/AnnotationLoader.php
+++ src/ConfigLoader/AnnotationLoader.php
@@ -1,12 +1,44 @@
 <?php
+/** @noinspection PhpDocMissingThrowsInspection */
 declare(strict_types=1);
 
 namespace felfactory\ConfigLoader;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use felfactory\Annotation\Shape;
+use ReflectionClass;
+
 class AnnotationLoader
 {
+    /** @var AnnotationReader */
+    protected $reader;
+
+    public function __construct()
+    {
+        /** @noinspection PhpDeprecationInspection */
+        AnnotationRegistry::registerLoader('class_exists');
+        $this->reader = new AnnotationReader();
+    }
+
+    /**
+     * @param string $className
+     * @psalm-param class-string $className
+     * @return string[]
+     * @psalm-return array<string, string>
+     */
     public function load(string $className): array
     {
+        $reflection = new ReflectionClass($className);
+        $properties = $reflection->getProperties();
+        $config     = [];
+        foreach ($properties as $property) {
+            $shape = $this->reader->getPropertyAnnotation($property, Shape::class);
+            if ($shape && $shape->shape) {
+                $config[$property->getName()] = $shape->shape;
+            }
+        }
 
+        return $config;
     }
 }
--- tests/TestModels/SimpleInvalidAnnotationTestModel.php
+++ tests/TestModels/SimpleInvalidAnnotationTestModel.php
@@ -2,23 +2,24 @@
 declare(strict_types=1);
 
 namespace felfactory\tests\TestModels;
+use felfactory\Annotation as Factory;
 
 class SimpleInvalidAnnotationTestModel
 {
     /**
-     * @Factory\Shape("firstName")
+     * @Factory\Shape()
      * @var string
      */
     public $firstName;
 
     /**
-     * @Factory\Shape("lastName")
+     * @Factory\Shape()
      * @var string
      */
     protected $lastName;
 
     /**
-     * @Factory\Shape("numberBetween(18, 59)")
+     * @Factory\Shape()
      * @var int
      */
     private $age;
